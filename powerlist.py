33fdca406fc0bc3c6dbc1f0f617329fe1e72f76f
Nicholas Stout <nsstout@iastate.edu>
2023-08-26 00:58:55 -0500

    bug fixes

--- powerlist.py
+++ powerlist.py
@@ -5,6 +5,7 @@
 
 
 from flask import Flask
+from flask import request
 from flask_sqlalchemy import SQLAlchemy
 
 # create the extension
@@ -76,25 +77,29 @@ with app.app_context():
 
 @app.route("/users")
 def user_list():
-    users = db.session.execute(db.select(User).order_by(User.username)).scalars()
-    return render_template("user/list.html", users=users)
+    #users = db.session.execute(db.select(User).order_by(User.username)).scalars()
+    users = User.query.all()
+    #return render_template("user/list.html", users=users)
+    print(users[0])
+    users = [{'id':u.id, 'name':u.username} for u in users]
+    return users
 
 @app.route("/users/create", methods=["GET", "POST"])
 def user_create():
     if request.method == "POST":
         user = User(
-            username=request.form["username"],
-            email=request.form["email"],
+            username=request.get_json()["username"],
         )
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for("user_detail", id=user.id))
+        return str(user.id)
 
     return render_template("user/create.html")
 
 @app.route("/user/<int:id>")
 def user_detail(id):
-    user = db.get_or_404(User, id)
+    #user = db.get_or_404(User, id)
+    User.query.filter_by(id=id).all()
     return render_template("user/detail.html", user=user)
 
 @app.route("/user/<int:id>/delete", methods=["GET", "POST"])
